c7075c11d00b86164ba997c8178b32044a73a18b
giggiox <luigi.cennini@libero.it>
2024-02-28 19:38:12 +0100

    somewhat working CUDA version

--- cuda/prova.py
+++ cuda/prova.py
@@ -1,26 +1,43 @@
-p = [2.3,4.2,4,2,1,6.5,5,75.3,2.54,23.2]
-centroids = [23.2,2.3]
-import math
-def edist(x1,x2):
-    return math.sqrt((x2-x1)*(x2-x1))
+from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+import os
 
 
-clusterLabels = []
-def clustAssing():
-    for i in range(len(p)):
-        minDist = math.inf
-        clusterLabel = 0
-        for j in range(len(centroids)):
-            distance = edist(p[i],centroids[j])
-            if distance < minDist:
-                minDist = distance
-                clusterLabel = j
-
-        clusterLabels.append(clusterLabel);
-
-clustAssing()
-print(clusterLabels)
+centroids = [[2.39014695, -0.57684421,-10],[-7.53619806,  4.49955772,13.4]]
 
 
-#index: 0 distance: 20.900002 j: 0  dataPoint: 2.300000 centroid: 23.200001 
-print(edist(2.300000,23.200001 ))
+def generate_blob_dataset(num_samples, num_clusters):
+    x, _ = make_blobs(n_samples=num_samples, n_features=3, centers=num_clusters)
+    return x
+
+def save_to_csv(x, filename):
+    df = pd.DataFrame(x)
+    df.to_csv(filename, header=False, index=False)
+
+
+
+
+p = generate_blob_dataset(1000,2)
+save_to_csv(p,"ciao.csv")
+X = np.array(p)
+initial_centroids = np.array(centroids)
+k = initial_centroids.shape[0]
+kmeans = KMeans(n_clusters=k, init=initial_centroids,max_iter=1)
+
+kmeans.fit(X)
+
+# Get the cluster centers
+centroids = kmeans.cluster_centers_
+
+# Get the cluster labels
+labels = kmeans.labels_
+
+print("Custom Initial Centroids:")
+print(initial_centroids)
+print("\nCluster centers:")
+print(centroids)
+print("\nCluster labels:")
+print(labels)
